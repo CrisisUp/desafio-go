fca1cf63a1372fec74be9dfa7b41b0a31f0eee1d
Cristiano <cristiano.1973.on@gmail.com>
2024-08-01 16:58:20 -0300

    second commit

--- main.go
+++ main.go
@@ -5,7 +5,30 @@ import "fmt"
 func main() {
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 {
-			fmt.Println(i)
+			fmt.Printf("%d ", i)
 		}
 	}
+	fmt.Println("\n===============================================================================================")
+
+	const reset = "\033[0m"
+	const green = "\033[32m"
+	const red = "\033[31m"
+	const yellow = "\033[33m"
+
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Print(red + "pin-pan " + reset)
+		} else if i%3 == 0 {
+			fmt.Print(green + "pin " + reset)
+		} else if i%5 == 0 {
+			fmt.Print(yellow + "pan " + reset)
+		} else {
+			fmt.Printf(" %d ", i)
+		}
+		if i%20 == 0 {
+			fmt.Println()
+		}
+	}
+	fmt.Println()
+
 }
